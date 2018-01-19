6f27d2a0b8d4768a59245cf5e50a454200a7c926
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-19 10:59:56 +0000

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+void main()
+{  
+    int count;
+    printf("oddnum between 1 to 1000");  
+    for(count = 1; count <= 1000; count++) 
+    {  
+  
+        if(count%2 == 0) 
+        { 
+        
+            printf("%d ",count);  
+        }  
+    }  
+   
+return 0; 
