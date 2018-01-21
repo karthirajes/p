9e470dbd3ca8f96bd83bbede7a072f012e8719a2
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 15:03:48 +0530

    Update and rename 617.c to 618.c

--- 618.c
+++ 618.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+void main() {
 	int n,k,m,l,a;
 	scanf("%d",&n);
 	k=n%10;
@@ -8,5 +8,5 @@ int main() {
 	l=m%10;
 	a=m/10;
 	printf("the individual digits are %d\t%d\t%d\t",a,l,k);
-	return 0;
+
 }
