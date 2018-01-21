07e9ce26c945e926ebc2cc907c1b2f454506fa1a
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 14:13:21 +0530

    Update and rename 550.c to 551.c

--- 550.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    int l,b,h,v;
-    scanf("%d %d %d",&l,&b,&h);
-    v=l*b*h;
-    printf("%d",v);
-    }
---
+++ 551.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    int l,b,h,n;
+    scanf("%d %d %d",&l,&n,&h);
+    v=l*n*h;
+    printf("%d",v);
+    }
