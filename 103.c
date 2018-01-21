081dfb0ecb307d9a8f5a054ff1fbb1e55b9c1cc7
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 13:26:54 +0530

    Create 103.c

---
+++ 103.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+int a[200],n,i,b,max;
+printf("\n enter the no. of values");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(b=0;b<n;b++)
+{
+	if(a[b]>a[i])
+		max=a[j];
+}
+}
+printf("\n the max value is %d ", max);
+	return 0;
+}
