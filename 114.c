7aeafd2d2b96b9904e4ad1c1d9ac3870035bf8cd
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 13:48:44 +0530

    Create 114.c

---
+++ 114.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int N,K,i,a[100],count=0;
+scanf("%d %d",&N,&K);
+for(i=0;i<N;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<N;i++)
+{
+if(a[i]==K)
+count++;
+}
+if(count!=0)
+printf("yes");
+else
+printf("no");
+return 0;
+}
