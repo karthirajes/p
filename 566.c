8ce4a7c0023da72719782410844d8740238d1e8b
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 14:26:00 +0530

    Update and rename 565.c to 566.c

--- 566.c
+++ 566.c
@@ -2,9 +2,9 @@
 
 void main()
 {
-    int 4,b,h,l;
-    scanf("%d %d %d",&4,&l,&h);
-    v=4*l*h;
+    int 4,b,a,l;
+    scanf("%d %d %d",&4,&l,&a);
+    v=4*l*a;
     printf("%d",v);
     return 0;
     }
