48e6dabcb579994678e536593a6adef2644a741f
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 14:18:12 +0530

    Update and rename 5521.c to 5511.c

---
+++ 5511.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    int 3,b,h,m;
+    scanf("%d %d %d",&3,&m,&h);
+    v=3*m*h;
+    printf("%d",v);
+    }
--- 5521.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    int 3,b,h,n;
-    scanf("%d %d %d",&3,&n,&h);
-    v=3*n*h;
-    printf("%d",v);
-    }
