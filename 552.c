ce4477fbc3aa251958f4ce64c5bdf158b5644fae
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 14:19:59 +0530

    Update and rename 552.c to 553.c

--- 552.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    int 4,b,h,m;
-    scanf("%d %d %d",&4,&m,&h);
-    v=4*m*h;
-    printf("%d",v);
-    }
---
+++ 553.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    int 4,b,h,k;
+    scanf("%d %d %d",&4,&k,&h);
+    v=4*k*h;
+    printf("%d",v);
+    }
