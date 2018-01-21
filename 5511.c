44331832c4210997278d6fc3bd7c31e4543c409d
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 14:19:04 +0530

    Update and rename 5511.c to 552.c

--- 5511.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    int 3,b,h,m;
-    scanf("%d %d %d",&3,&m,&h);
-    v=3*m*h;
-    printf("%d",v);
-    }
---
+++ 552.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    int 4,b,h,m;
+    scanf("%d %d %d",&4,&m,&h);
+    v=4*m*h;
+    printf("%d",v);
+    }
