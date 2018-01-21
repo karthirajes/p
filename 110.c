7c646704de93910c52444771fb6c180e051486d4
karthirajes <35220312+karthirajes@users.noreply.github.com>
2018-01-21 13:43:13 +0530

    Create 110.c

---
+++ 110.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int min,hour,min;
+printf("enter the  min");
+scanf("%d",&min);
+hour=min/60;
+min=min%60;
+printf("HOUR\t\tMIN\n");
+printf("%d\t\t%d",hour,min);
+}
